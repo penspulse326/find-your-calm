ae426edd2674e3090be5f899d7f2f14f323cca89
Shin <penspulse@gmail.com>
2026-03-11 16:04:26 +0800

    feat: add assets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,14 +16,18 @@ function startQuiz() {
 </script>
 
 <template>
-  <div class="flex-1 flex flex-col items-center justify-center p-8 text-center bg-gradient-to-b from-gray-800 to-black text-white relative h-full">
+  <div
+    class="flex-1 flex flex-col items-center justify-center p-8 text-center bg-gradient-to-b from-gray-800 to-black text-white relative h-full"
+  >
     <div class="space-y-6 z-10 w-full">
-      <h1 class="text-3xl font-serif font-bold tracking-widest text-white/90 mb-2">
+      <h1
+        class="text-3xl font-serif font-bold tracking-widest text-white/90 mb-2"
+      >
         尋找平靜
       </h1>
       <div class="w-12 h-[1px] bg-white/30 mx-auto my-4" />
       <p class="text-white/60 leading-relaxed text-sm max-w-xs mx-auto">
-        透過情境代入感評估焦慮程度，並獲得科學依據的改善建議。
+        你有多久沒有好好放鬆了呢？透過幾個日常問題，慢慢找到通往平靜的方向吧！
       </p>
       <div class="pt-8">
         <button
